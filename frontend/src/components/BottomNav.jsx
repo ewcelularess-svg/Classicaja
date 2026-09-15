df66e3a6cf5acd2a9f5343440aa1f6035acab95a
ewcelularess-svg <ewcelularess@gmail.com>
2026-09-15 12:30:18 -0300

    Add files via upload

--- frontend/src/components/BottomNav.jsx
+++ frontend/src/components/BottomNav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Home, LayoutDashboard, MessageCircle, PlusCircle, ShieldCheck, UserCircle} from 'lucide-react';
+import {Home, LayoutDashboard, MessageCircle, PlusCircle, UserCircle} from 'lucide-react';
 import {NavLink} from 'react-router-dom';
 import {useAuth} from '../main';
 
@@ -7,9 +7,7 @@ export default function BottomNav(){
  const {user}=useAuth();
  return <nav className="bottom-nav">
   <NavLink to="/"><Home/><span>Início</span></NavLink>
-  {user?.role==='admin'
-    ? <NavLink to="/admin"><ShieldCheck/><span>Master</span></NavLink>
-    : <NavLink to="/painel"><LayoutDashboard/><span>Painel</span></NavLink>}
+  <NavLink to={user?'/painel':'/entrar'}><LayoutDashboard/><span>Painel</span></NavLink>
   <NavLink className="sell" to={user?'/escolher-plano':'/entrar'}><PlusCircle/><span>Anunciar</span></NavLink>
   <NavLink to="/mensagens"><MessageCircle/><span>Chat</span></NavLink>
   <NavLink to="/meus-anuncios"><UserCircle/><span>Anúncios</span></NavLink>
